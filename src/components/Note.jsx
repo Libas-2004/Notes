99fc233b9160e83808e5bdad384ae24612cd70c1
Libas-2004 <nellailibas2004@gmail.com>
2024-07-09 15:58:04 +0530

    first commit

---
+++ src/components/Note.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div>
+    <div className="note">
+      <h1>Type as you wish</h1>
+      <p contentEditable="true">This is the note content</p>
+    </div>
+    <div className="note">
+      <h1>Type as you wish</h1>
+      <p contentEditable="true">This is the note content</p>
+    </div>
+    <div className="note">
+      <h1>Type as you wish</h1>
+      <p contentEditable="true">This is the note content</p>
+    </div>
+    
+    </div>
+    
+  );
+}
+
+export default Note;
